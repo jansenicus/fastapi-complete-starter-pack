665865ad3763da9f3a254ef8bc531eb578956b44
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-01-29 15:00:27 +0700

    Update index.py

--- fabulous/apis/pages/index.py
+++ fabulous/apis/pages/index.py
@@ -10,5 +10,4 @@ index = APIRouter()
 
 @index.get("/")
 async def home(request: Request, style: str = 'default'):
-	return templates.TemplateResponse("index.html",context = {"request":request, "style": style})
-	
+	return templates.TemplateResponse("index.html", context = {"request":request, "style": style})
