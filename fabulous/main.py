1cc67a69d295ebe94e45828bba32c6fb738dedf7
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-04 19:50:52 +0700

    Update main.py

--- fabulous/main.py
+++ fabulous/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from core.settings import config
 from fastapi.staticfiles import StaticFiles
-from apis.pages.index import index
+from apis.index import index
 
 app = FastAPI(title=config.PROJECT_NAME, version=config.PROJECT_VERSION)
 app.mount("/static", StaticFiles(directory="static"), name="static")
