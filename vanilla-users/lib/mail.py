2728f7977bca3f4c7d37eef18dcdbb29f01250da
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-13 21:48:21 +0700

    added new line
    added new line

--- vanilla-users/lib/mail.py
+++ vanilla-users/lib/mail.py
@@ -8,6 +8,7 @@ import os
 load_dotenv(Path('lib/mail.conf'))
 
 mailconfig = ConnectionConfig(
+    
     MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD'),
     MAIL_FROM = os.getenv('MAIL_FROM'),
